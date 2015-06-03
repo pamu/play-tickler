79670f3bc0bfb1fe345a15133271dd9f78e3d4f2
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-06-03 23:43:02 +0530

    added db

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -1,14 +1,21 @@
 package models
 
+import java.net.URI
+
 import slick.driver.MySQLDriver.api._
 /**
  * Created by pnagarjuna on 26/05/15.
  */
 object DB {
+  val uri = new URI(s"""postgres://cepjpsajfsbvxz:gc5Htv7jWwb_oPExSd6Tz3ERSz@ec2-54-83-25-238.compute-1.amazonaws.com:5432/daos93l04q2jcl""")
+
+  val username = uri.getUserInfo.split(":")(0)
+
+  val password = uri.getUserInfo.split(":")(1)
+
   lazy val db = Database.forURL(
-    url = "jdbc:mysql://localhost/ticksys",
-    driver = "com.mysql.jdbc.Driver",
-    user="root",
-    password="root"
+    driver = "org.postgresql.Driver",
+    url = "jdbc:postgresql://" + uri.getHost + ":" + uri.getPort + uri.getPath, user = username,
+    password = password
   )
 }
