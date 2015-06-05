d16e08713a7f8cea2afc266a765891829b897328
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-06-05 19:50:50 +0530

    fixed

--- public/js/loader.js
+++ public/js/loader.js
@@ -6,14 +6,10 @@ function pageLoader(page, pageSize){
                 for(var i = 0; i < list.length; i++) {
                     $("#tickles").append('<div class="alert alert-success">' + list[i].text + '</div>')
                 }
-                return true;
             }
             if (data.failure) {
                 emsg('Error retrieving information, reason: ' + data.failure.reason)
-                return false;
             }
-            return false;
         }
     });
-    return false;
 }
